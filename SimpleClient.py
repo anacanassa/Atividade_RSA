cbc9bae2db5e67bc003152db9473a1217ef1764b
Erik Vieira <70588081+EForamiglio@users.noreply.github.com>
2026-09-08 21:21:42 -0300

    Melhorandoo logica do clientee e utils

--- SimpleClient.py
+++ SimpleClient.py
@@ -1,8 +1,9 @@
 from socket import *
 import Utils
+import ast
 
 #Informação de rede
-serverName = "10.1.70.9"
+serverName = "192.168.0.141"
 serverPort = 25000
 
 #Criar a conexão
@@ -11,14 +12,14 @@ clientSocket.connect((serverName,serverPort))
 
 #Definindo valores diffie-helman
 severInfo = str(clientSocket.recv(65000),"utf-8")
-chavePublicaServer = severInfo 
+chavePublicaServer = ast.literal_eval(severInfo) 
 chavePublicaClient, chavePrivadaClient = Utils.gen_rsa_key()
 
 mensagem = "The information security is of significant importance to ensure the privacy of communications"
 
 cripty_msg = Utils.crip_wtih_rsa(mensagem, chavePublicaServer)
 
-package = f"{chavePublicaClient},{mensagem}"
+package = f"{chavePublicaClient},{cripty_msg}"
 
 #Enviando chave e mensagem
 clientSocket.send(bytes(package, "utf-8"))
@@ -26,7 +27,7 @@ clientSocket.send(bytes(package, "utf-8"))
 modifiedSentence = clientSocket.recv(65000)
 text = str(modifiedSentence,"utf-8")
 
-decriptoModified = Utils.decrip_wtih_rsa(text, chavePrivadaClient)
+decriptoModified = Utils.decrip_wtih_rsa(ast.literal_eval(text), chavePrivadaClient)
 
 print ("Received from Make Upper Case Server: ", decriptoModified)
 clientSocket.close()
